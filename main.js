fa66d7e2989ae0a1b9abaeb5fbb982f1737b070f
iharri0601 <40372695+iharri0601@users.noreply.github.com>
2018-06-22 20:06:14 -0400

    Javascript Exercises Part 1

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("Hello Izaiah");
+var number1 = 20;
+var number2 = 30;
+var sum = number1 + number2;
+console.log(sum);
+var puppy = {
+  name: 'cassius',
+  color: 'brown',
+  age: 8
+};
+console.log(puppy);
+console.log(puppy.name);
+puppy.age = puppy.age + 4;
+console.log(puppy.age);
+puppy.weight = puppy.age + 10;
+console.log(puppy.weight);
+var warm = true;
+if (warm) {
+  console.log("wear shorts!");
+} else {
+  console.log("Make sure you grab a jacket!");
+};
+var height = 36;
+if(height < 52) {
+  console.log('Sorry. You can\'t ride the ride!');
+}
+"Hello" + "World"
