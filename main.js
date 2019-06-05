4510639ce3f27a287e95326e1b5897da75394c77
iharri0601 <40372695+iharri0601@users.noreply.github.com>
2019-06-05 17:27:47 -0400

    initial commit

--- main.js
+++ main.js
@@ -24,4 +24,45 @@ var height = 36;
 if(height < 52) {
   console.log('Sorry. You can\'t ride the ride!');
 }
-"Hello" + "World"
+
+
+
+var ten = 5 + 5;
+
+var twelve = 7 + 5;
+
+var fifteen = 10 + 5;
+
+function plusfive(number) {
+  console.log(number + 5);
+}
+
+var ten = plusfive(5);
+var twelve = plusfive(7);
+var fifteen = plusfive(10);
+
+function plusten(num) {
+  console.log(num + 10);
+  return num + 10;
+}
+
+var twenty = plusten(10);
+var thirty = plusten(20);
+
+var myconsole = {};
+
+myconsole.log = function(word) {
+  console.log(word + "from Alex ");
+}
+
+myconsole.log("hey ");
+
+var mysentence = {} ;
+function sentence (word1, word2) {
+  mysentence = word1 + word2;
+ console.log("Inside function: " + mysentence);
+return [mysentence, word1];
+}
+
+var speech = sentence("howdy ", "folks! ");
+console.log(speech);
